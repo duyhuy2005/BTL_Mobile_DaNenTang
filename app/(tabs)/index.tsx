3786117d229579e2962a7cb9d3a9a96e79b55ce2
duyhuy2005 <duyhuy2812@gmail.com>
2026-08-28 23:08:43 +0700

    Update HomeScreen component text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ export default function HomeScreen() {
   return (
     <>
       <View>
-        <Text>Đào huy đẩy code</Text>
+        <Text>Trần huy đẩy code</Text>
         <Button title="Press me!" onPress={() => alert('Huy day')} />
       </View>
     </>
